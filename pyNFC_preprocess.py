936848a9bb1d726c54e343d114ff074a39c85112
Stu Blair <stu314159@gmail.com>
2017-07-05 18:26:58 +0000

    fixed bugs in pre-processing script

--- pyNFC_preprocess.py
+++ pyNFC_preprocess.py
@@ -22,7 +22,7 @@ N_divs = 7
 
 geom_file_stub = 'sphere'
 sphereObst = fc.SphereObstruction(r_s,s_x,s_y,s_z)
-channel = fc.FluidChannel(Lx_p,Ly_p,Lz_p,
-                          N_divs,sphereObst)
+channel = fc.FluidChannel(Lx_p = Lx_p,Ly_p = Ly_p,Lz_p = Lz_p,
+                          N_divs = N_divs,obst = sphereObst, fluid = 'water')
 channel.write_bc_vtk()
 channel.write_mat_file(geom_file_stub)
