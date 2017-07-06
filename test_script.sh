bd3fb66143f2ea83679d4cb507cad0263e5093f0
Stu Blair <stu314159@gmail.com>
2017-07-06 13:08:40 -0500

    update to test_script.m

--- test_script.sh
+++ test_script.sh
@@ -10,7 +10,7 @@ python ./genInput.py sphere.mat D3Q19 12 1 0 10 10 1e-2 0 0
 python ./pyNFC2_test.py
 
 # gold standard is set on output from 10th time step
-python ./validate.py 1
+#python ./validate.py 1
 
 # post-process the output
 mpirun -np 4 ./pyNFC_postprocess.py
