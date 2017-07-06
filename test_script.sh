c0ead72a403de5cb47c0e8edc626f3721cf0ecef
Stu Blair <stu314159@gmail.com>
2017-07-06 11:26:09 -0500

    added bash script to automate testing

--- test_script.sh
+++ test_script.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# pre-process
+python ./pyNFC_preprocess.py
+
 # input
-python ./genInput.py sphere.mat D3Q15 25 1 0 1 10 1e-2 0 0
+python ./genInput.py sphere.mat D3Q15 100 1 0 10 10 1e-2 0 0
 
 # run the code
 python ./pyNFC2_test.py
+
+
+# post-process the output
+mpirun -np 4 ./pyNFC_postprocess.py
 
