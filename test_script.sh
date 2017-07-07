dd7cd4460fa96fd9facb0ae74eb4e07a27d80424
Stu Blair <stu314159@gmail.com>
2017-07-07 16:50:52 -0500

    made lattice type an argument to the test script

--- test_script.sh
+++ test_script.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+# $1 - lattice type
 
 # pre-process
 python ./pyNFC_preprocess.py
 
 # input
-python ./genInput.py sphere.mat D3Q19 12 1 0 10 10 1e-2 0 0
+python ./genInput.py sphere.mat $1 12 1 0 10 10 1e-2 0 0
 
 # run the code
 python ./pyNFC2_test.py
